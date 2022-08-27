d93e8336494246211aec20d83b3141d99a3eae6e
Ghidella <raphaelfghidella@gmail.com>
2022-08-27 08:19:39 -0300

    game mode and play interact implementation

--- minesweeper.c
+++ minesweeper.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #define MAX 30
-
+//game board
 typedef struct{
-    int table[MAX][MAX]; // GAME TABLE
+    int table[MAX][MAX];
     int gamemode;
     } TABLE;
 
 void tableInitializer(int game_mode, TABLE* game){
+    //refreshing pseudo-random numbers seed
     srand(time(NULL));
 
     //easy mode
@@ -64,24 +66,49 @@ void tableInitializer(int game_mode, TABLE* game){
 }
 
 void printTable(TABLE game){
+    system("cls");
     int line, column;
     if(game.gamemode == 0) line = 8, column = 10;
     else if(game.gamemode == 1) line = 14, column = 18;
     else line = 20, column = 24;
     for(int i = 0; i < line; i++){
         for(int j = 0; j < column; j++){
-            printf("%d ", game.table[i][j]);
+            printf("[] ");
         }
+
         printf("\n");
     }
 }
 
+bool checkMove(TABLE game, int linha, int coluna){
+    if(game.table[linha][coluna] == 0) return true;
+    else return false;
+}
+
+void makeMove(TABLE* game, int linha, int coluna){
+    game->table[linha][coluna] = 2;
+}
 
 int main() {
     TABLE game;
     printf("Choose gamemode:\n(0)Easy    (1)Medium    (2)Hard\n");
     scanf("%d", &game.gamemode);
+    fflush(stdin);
     tableInitializer(game.gamemode, &game);
-    printTable(game);
+    int i = 0;
+    while (i < MAX){
+        printTable(game);
+        printf("Choose a position by following Aij like a matrix:\n");
+        int line_move, column_move;
+        printf("Line: ");
+        scanf("%d", &line_move);
+        fflush(stdin);
+        printf("Column: ");
+        scanf("%d", &column_move);
+        fflush(stdin);
+        if(checkMove(game, line_move, column_move)== true) makeMove(&game, line_move, column_move);
+        else break;
+    }
+    printf("You Loose!");
     return 1;
 }
